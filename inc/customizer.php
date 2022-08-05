24243aaf83e388c118565a5282abc26b82ef9f34
Matt B <mattia.bonacini@gmail.com>
2022-08-05 21:21:17 +0300

    Corrected PHP global version variable

--- inc/customizer.php
+++ inc/customizer.php
@@ -56,6 +56,6 @@ function awesome_forms_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function awesome_forms_customize_preview_js() {
-	wp_enqueue_script( 'awesome-forms-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+	wp_enqueue_script( 'awesome-forms-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), AF_VERSION, true );
 }
 add_action( 'customize_preview_init', 'awesome_forms_customize_preview_js' );
